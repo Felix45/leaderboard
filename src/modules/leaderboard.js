c4864da596235dac0f3a6ddf79b456859e5dc682
Felix Ouma <fatonoh@gmail.com>
2022-06-15 14:28:37 +0300

    Add leaderboard addscore and getscore functions

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,17 +1,41 @@
-const scores = [
-  { name: 'Felix Ouma', score: 100 },
-  { name: 'Richard Leakey', score: 50 },
-  { name: 'Isaiah Thomas', score: 40 },
-  { name: 'Joel Cenas', score: 25 },
-  { name: 'Charles Brandsam', score: 100 },
-  { name: 'Kennedy Stam', score: 50 },
-  { name: 'Lambert Kizito', score: 40 },
-  { name: 'Alice Wonderland', score: 25 },
-];
+const scores = [];
+const gameId = '77Dk40FTlaYbEU2kfdMk';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
-const board = scores.reduce((prev, current) => {
-  prev += `<li>${current.name} ${current.score}</li>`;
-  return prev;
-}, '');
+async function createGame() {
+   const result = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({name: 'Space shooters'}),
+   }).then((response) => response.json()).then((data) => console.log(data));
+}
 
-export default board;
+async function getScores() {
+  let scoreUrl = url + gameId +'/scores/';
+  const scores = await fetch(scoreUrl).then((response) => response.json());
+  console.log(scores);
+}
+getScores();
+
+async function addScore(user, score) {
+  let scoreUrl = url + gameId +'/scores/';
+  const result = await fetch(scoreUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({user, score}),
+  }).then((response) => response.json());
+}
+
+const displayBoard = (scores) => {
+  const board = scores.reduce((prev, current) => {
+    prev += `<li>${current.name} ${current.score}</li>`;
+    return prev;
+  }, '');
+  return board;
+}
+
+export { getScores, addScore, displayBoard };
