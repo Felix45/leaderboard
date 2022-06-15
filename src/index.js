e02166d51bba43074db1e712a0d6334f33d46cfd
Felix Ouma <fatonoh@gmail.com>
2022-06-15 19:01:37 +0300

    Refactor project to use classes and arrow functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import './style.css';
-import { addScore, getScores } from './modules/leaderboard.js';
+import LeaderBoard from './modules/leaderboard.js';
 
 const refreshBtn = document.querySelector('.refresh');
 const addForm = document.forms[0];
 
-getScores();
+const leaderBoard = new LeaderBoard();
+leaderBoard.getScores();
 
-refreshBtn.addEventListener('click', getScores);
+refreshBtn.addEventListener('click', leaderBoard.getScores);
 
 addForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  addScore(addForm.user.value, addForm.score.value);
+  leaderBoard.addScore(addForm.user.value, addForm.score.value);
   addForm.reset();
 });
--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,51 +1,55 @@
-const gameId = 'Y5PURJEgHZgvF0sESt7W';
-const alertBox = document.querySelector('.alert');
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+class LeaderBoard {
+  constructor() {
+    this.gameId = 'Y5PURJEgHZgvF0sESt7W';
+    this.alertBox = document.querySelector('.alert');
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  }
 
-async function createGame() {
-  await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name: 'Space shooters' }),
-  }).then((response) => response.json()).then((data) => data);
+  createGame = async () => {
+    await fetch(LeaderBoard.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'Space shooters' }),
+    }).then((response) => response.json()).then((data) => data);
+  }
+
+  displayBoard = async (data) => {
+    const board = data.reduce((prev, current) => {
+      prev += `<li>${current.user} : ${current.score}</li>`;
+      return prev;
+    }, '');
+    document.querySelector('.leaderboard').innerHTML = board;
+  };
+
+  getScores = async () => {
+    const scoreUrl = `${this.url + this.gameId}/scores/`;
+    await fetch(scoreUrl).then((response) => response.json())
+      .then((data) => {
+        this.displayBoard(data.result);
+      });
+  }
+
+  addScore = async (user, score) => {
+    const scoreUrl = `${this.url + this.gameId}/scores/`;
+    await fetch(scoreUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user, score }),
+    }).then((response) => response.json())
+      .then((data) => {
+        this.getScores();
+        this.alertBox.innerHTML = data.result;
+        this.alertBox.classList.toggle('d-none');
+      });
+
+    setTimeout(() => {
+      this.alertBox.classList.toggle('d-none');
+    }, 2000);
+  }
 }
 
-const displayBoard = (data) => {
-  const board = data.reduce((prev, current) => {
-    prev += `<li>${current.user} : ${current.score}</li>`;
-    return prev;
-  }, '');
-  document.querySelector('.leaderboard').innerHTML = board;
-};
-
-async function getScores() {
-  const scoreUrl = `${url + gameId}/scores/`;
-  await fetch(scoreUrl).then((response) => response.json())
-    .then((data) => {
-      displayBoard(data.result);
-    });
-}
-
-async function addScore(user, score) {
-  const scoreUrl = `${url + gameId}/scores/`;
-  await fetch(scoreUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ user, score }),
-  }).then((response) => response.json())
-    .then((data) => {
-      getScores();
-      alertBox.innerHTML = data.result;
-      alertBox.classList.toggle('d-none');
-    });
-
-  setTimeout(() => {
-    alertBox.classList.toggle('d-none');
-  }, 2000);
-}
-
-export { getScores, addScore };
+export default LeaderBoard;
