5a04a054242f2ff432a3647daffe268db7a05a1e
Felix Ouma <fatonoh@gmail.com>
2022-06-15 14:20:46 +0300

    Add babel compiler to project

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
     ],
   },
 };
